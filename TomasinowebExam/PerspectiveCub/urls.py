cae9341eb66f87de26e5ac1a0f21eb4e0f743463
nigelhaim <nigelsebastian21@gmail.com>
2023-07-17 20:46:57 +0800

    Working registration page

--- TomasinowebExam/PerspectiveCub/urls.py
+++ TomasinowebExam/PerspectiveCub/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("logout", views.logout_view, name="logout"),
+    path("login", views.login_view, name="login"),
+    path("signup", views.signup_view, name="signup")
 ]
